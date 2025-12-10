98f4eded756c3441f610a91041bc93beb8159f0e
fachrialifiandi <aliffachrimuhamad@gmail.com>
2025-12-10 23:06:07 +0700

    Update main.py

--- main.py
+++ main.py
@@ -7,82 +7,84 @@ import os
 
 from gudang_utils.functions import *
 
-def main():    
-    os.system('cls')
-    # Penciptaan array Kode Barang, Nama Barang, Jumlah Barang, dan Satuan Barang
-    Kode = ['/'] * MAKSBARANG
-    Nama = ['/'] * MAKSBARANG
-    Jumlah = [0] * MAKSBARANG
-    Satuan = ['/'] * MAKSBARANG
+class PengelolaanGudang:
 
-    KodeBaru = ['/'] * MAKSBARANG
-    NamaBaru = ['/'] * MAKSBARANG
-    JumlahBaru = [0] * MAKSBARANG
-    SatuanBaru = ['/'] * MAKSBARANG
-
-    N = 0
-    Menu = 0
-    Menu = MenuUtama(Menu)
-    while (Menu != 0):
+    def main():    
         os.system('cls')
-        match (Menu):
-            case 1:
-                N = PenerimaanBarang(Kode, Nama, Jumlah, Satuan, N)
-
-            case 2:
-                if N < MAKSBARANG :
-                    print("<< PENAMBAHAN BARANG >>")
-                    print ()
-                    KodeBaru = input('Kode Barang   : ').upper()
-                    NamaBaru = input('Nama Barang   : ').upper()
-                    JumlahBaru = int(input('Jumlah Barang : '))
-                    SatuanBaru = input('Satuan Barang : ').upper()
-                N = TambahBarang(Kode, Nama, Jumlah, Satuan, N, KodeBaru, NamaBaru, JumlahBaru, SatuanBaru)
-                
-            case 3:
-                TampilBarang(Kode, Nama, Jumlah, Satuan, N)
-                
-            case 4:
-                    if (N == 0):
-                        print("Gudang kosong, tidak ada barang dapat dikeluarkan.")
-                        os.system('pause')
-                    else:
-                        print("<< BARANG YANG KELUAR >>")
-                        print()
-                        ElemenHapus = input('Masukkan kode barang yang akan dikeluarkan: ').upper()
-                        N = HapusBarang(Kode, Nama, Jumlah, Satuan, N, ElemenHapus)
-                
-            case 5:
-                Menu2 = 0
-                Menu2 = MenuPengurutan(Menu2)
-                while (Menu2 != 0):
-                    os.system('cls')
-                    match (Menu2):
-                        case 1 :
-                            print('<< PENGURUTAN NAMA SECARA ASCENDING >>')
-                            BarangNamaAsc(Kode, Nama, Jumlah, Satuan, N)
-                            os.system('cls')
-                            TampilBarang(Kode, Nama, Jumlah, Satuan, N)
-                        case 2 :
-                            print('<< PENGURUTAN KODE SECARA DESCENDING >>')
-                            BarangKodeDsc(Kode, Nama, Jumlah, Satuan, N)
-                            os.system('cls')
-                            TampilBarang(Kode, Nama, Jumlah, Satuan, N)
-                            
-                    
-                    os.system('cls')
-                    Menu2 = MenuPengurutan(Menu2) 
-                    
-            case 6:
-                print("     << PENCARIAN BARANG >>")
-                print("")
-                SequentialSearch(Kode, Nama, Jumlah, Satuan, N)
-                
-            case 7 :
-                print("<< Penghancuran Data Barang >>")
-                MenghapusSemuaBarangData(Kode, Nama, Jumlah, Satuan, N, KodeBaru, NamaBaru, JumlahBaru, SatuanBaru)
-                
-
+        # Penciptaan array Kode Barang, Nama Barang, Jumlah Barang, dan Satuan Barang
+        Kode = ['/'] * MAKSBARANG
+        Nama = ['/'] * MAKSBARANG
+        Jumlah = [0] * MAKSBARANG
+        Satuan = ['/'] * MAKSBARANG
+    
+        KodeBaru = ['/'] * MAKSBARANG
+        NamaBaru = ['/'] * MAKSBARANG
+        JumlahBaru = [0] * MAKSBARANG
+        SatuanBaru = ['/'] * MAKSBARANG
+    
+        N = 0
+        Menu = 0
         Menu = MenuUtama(Menu)
-if __name__ == "__main__":
-    main()
+        while (Menu != 0):
+            os.system('cls')
+            match (Menu):
+                case 1:
+                    N = PenerimaanBarang(Kode, Nama, Jumlah, Satuan, N)
+    
+                case 2:
+                    if N < MAKSBARANG :
+                        print("<< PENAMBAHAN BARANG >>")
+                        print ()
+                        KodeBaru = input('Kode Barang   : ').upper()
+                        NamaBaru = input('Nama Barang   : ').upper()
+                        JumlahBaru = int(input('Jumlah Barang : '))
+                        SatuanBaru = input('Satuan Barang : ').upper()
+                    N = TambahBarang(Kode, Nama, Jumlah, Satuan, N, KodeBaru, NamaBaru, JumlahBaru, SatuanBaru)
+                    
+                case 3:
+                    TampilBarang(Kode, Nama, Jumlah, Satuan, N)
+                    
+                case 4:
+                        if (N == 0):
+                            print("Gudang kosong, tidak ada barang dapat dikeluarkan.")
+                            os.system('pause')
+                        else:
+                            print("<< BARANG YANG KELUAR >>")
+                            print()
+                            ElemenHapus = input('Masukkan kode barang yang akan dikeluarkan: ').upper()
+                            N = HapusBarang(Kode, Nama, Jumlah, Satuan, N, ElemenHapus)
+                    
+                case 5:
+                    Menu2 = 0
+                    Menu2 = MenuPengurutan(Menu2)
+                    while (Menu2 != 0):
+                        os.system('cls')
+                        match (Menu2):
+                            case 1 :
+                                print('<< PENGURUTAN NAMA SECARA ASCENDING >>')
+                                BarangNamaAsc(Kode, Nama, Jumlah, Satuan, N)
+                                os.system('cls')
+                                TampilBarang(Kode, Nama, Jumlah, Satuan, N)
+                            case 2 :
+                                print('<< PENGURUTAN KODE SECARA DESCENDING >>')
+                                BarangKodeDsc(Kode, Nama, Jumlah, Satuan, N)
+                                os.system('cls')
+                                TampilBarang(Kode, Nama, Jumlah, Satuan, N)
+                                
+                        
+                        os.system('cls')
+                        Menu2 = MenuPengurutan(Menu2) 
+                        
+                case 6:
+                    print("     << PENCARIAN BARANG >>")
+                    print("")
+                    SequentialSearch(Kode, Nama, Jumlah, Satuan, N)
+                    
+                case 7 :
+                    print("<< Penghancuran Data Barang >>")
+                    MenghapusSemuaBarangData(Kode, Nama, Jumlah, Satuan, N, KodeBaru, NamaBaru, JumlahBaru, SatuanBaru)
+                    
+    
+            Menu = MenuUtama(Menu)
+    if __name__ == "__main__":
+        main()
